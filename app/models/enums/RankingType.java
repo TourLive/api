d0bc5992d8b115d542b6e103904daae4f42b71bc
Hawner <dominik_good@hotmail.com>
2018-03-22 20:33:48 +0100

    enums added

---
+++ app/models/enums/RankingType.java
@@ -0,0 +1,5 @@
+package models.enums;
+
+public enum RankingType {
+    VIRTUAL, OFFICIAL, MOUNTAIN, SPRINT, SWISS
+}
---
+++ app/models/enums/RewardType.java
@@ -0,0 +1,5 @@
+package models.enums;
+
+public enum RewardType {
+    POINTS, TIME
+}
