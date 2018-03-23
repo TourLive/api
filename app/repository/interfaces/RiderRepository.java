82076cb88513080d230acb35a20ca0b5dd0fb815
Hawner <dominik_good@hotmail.com>
2018-03-23 15:07:09 +0100

    Update RiderRepo Interface

--- app/repository/interfaces/RiderRepository.java
+++ app/repository/interfaces/RiderRepository.java
@@ -9,5 +9,9 @@ import java.util.stream.Stream;
 
 @ImplementedBy(RiderRespositoryImpl.class)
 public interface RiderRepository {
-    CompletionStage<Stream<Rider>> list();
+    CompletionStage<Stream<Rider>> getAllRiders();
+    void addRider(CompletionStage<Rider> rider);
+    CompletionStage<Rider> getRider(int riderId);
+    void deleteAllRiders();
+    void deleteRider(int riderId);
 }
