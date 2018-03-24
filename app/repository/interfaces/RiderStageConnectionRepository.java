358202d3cb96008b0a52c072eff78c9f06373745
Hawner <dominik_good@hotmail.com>
2018-03-24 08:54:18 +0100

    RiderStageConnection Repo added

--- app/repository/interfaces/RiderStageConnectionRepository.java
+++ app/repository/interfaces/RiderStageConnectionRepository.java
@@ -1,14 +1,17 @@
 package repository.interfaces;
 
+import com.google.inject.ImplementedBy;
 import models.RiderStageConnection;
+import repository.RiderStageConnectionRepositoryImpl;
 
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 
+@ImplementedBy(RiderStageConnectionRepositoryImpl.class)
 public interface RiderStageConnectionRepository {
     CompletionStage<Stream<RiderStageConnection>> getAllRiderStageConnections();
-    void addRiderStageConnection(CompletionStage<RiderStageConnection> riderStageConnection);
     CompletionStage<RiderStageConnection> getRiderStageConnectionByRiderAndStage(int riderId, int stageId);
+    void addRiderStageConnection(RiderStageConnection riderStageConnection);
     void deleteAllRiderStageConnections();
     void deleteRiderStageConnection(int riderId, int stageId);
 }
