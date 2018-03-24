e6eb0fbcedcd1f59ffb0dd8971f3f71aa165e274
Hawner <dominik_good@hotmail.com>
2018-03-24 10:17:34 +0100

    Judgment Repo added

--- app/repository/interfaces/JudgmentRepository.java
+++ app/repository/interfaces/JudgmentRepository.java
@@ -1,14 +1,17 @@
 package repository.interfaces;
 
+import com.google.inject.ImplementedBy;
 import models.Judgment;
+import repository.JudgmentRepositoryImpl;
 
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 
+@ImplementedBy(JudgmentRepositoryImpl.class)
 public interface JudgmentRepository {
     CompletionStage<Stream<Judgment>> getAllJudgments();
     CompletionStage<Stream<Judgment>> getJudgmentsByRider(int riderId);
-    void addJudgment(CompletionStage<Judgment> judgment);
+    void addJudgment(Judgment judgment);
     void deleteAllJudgment();
     void deleteJudgmentByJudgmentName(String judgmentName);
 }
