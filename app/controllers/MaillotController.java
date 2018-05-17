2732b09e5b3810f828ee6152817679b5d2fb43f0
Hawner <dominik_good@hotmail.com>
2018-05-17 11:07:49 +0200

    MaillotController Caching

--- app/controllers/MaillotController.java
+++ app/controllers/MaillotController.java
@@ -7,6 +7,8 @@ import io.swagger.annotations.ApiResponses;
 import models.Maillot;
 import models.MaillotDTO;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import play.api.libs.ws.ahc.cache.CachingAsyncHttpClient;
+import play.cache.AsyncCacheApi;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repository.interfaces.MaillotRepository;
@@ -24,16 +26,18 @@ public class MaillotController extends Controller {
     private final MaillotRepository maillotRepository;
     private static final String INDEXOUTOFBOUNDEXCEPETION = "IndexOutOfBoundsException";
     private static final String NULLPOINTEREXCEPTION = "NullPointerException";
+    private static final int CACHE_DURATION = 10;
+    private final AsyncCacheApi cache;
 
     @Inject
-    public MaillotController(MaillotRepository maillotRepository) { this.maillotRepository = maillotRepository; }
+    public MaillotController(MaillotRepository maillotRepository, AsyncCacheApi cache) { this.maillotRepository = maillotRepository; this.cache = cache; }
 
     @ApiOperation(value ="get all maillots of a stage", response = MaillotDTO.class, responseContainer = "List")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "No maillots are set in DB for this stage."),
             @ApiResponse(code = 500, message = "Error on getting maillots of the specified stage") })
     public CompletionStage<Result> getMaillots(Long stageId) {
-        return maillotRepository.getAllMaillots(stageId).thenApplyAsync(maillots -> {
+        return cache.getOrElseUpdate("maillots/"+stageId, () -> maillotRepository.getAllMaillots(stageId).thenApplyAsync(maillots -> {
             List<MaillotDTO> maillotDTOList = new ArrayList<>();
             for (Maillot m : maillots.collect(Collectors.toList())) {
                 maillotDTOList.add(new MaillotDTO(m));
@@ -47,7 +51,7 @@ public class MaillotController extends Controller {
                 res = internalServerError(ex.getMessage());
             }
             return res;
-        });
+        }), CACHE_DURATION);
     }
 
     @ApiOperation(value ="get maillot by id", response = MaillotDTO.class)
@@ -55,7 +59,7 @@ public class MaillotController extends Controller {
             @ApiResponse(code = 400, message = "No specific maillot is set in DB for this id."),
             @ApiResponse(code = 500, message = "Error on getting maillot by id") })
     public CompletionStage<Result> getMaillot(Long maillotId) {
-        return maillotRepository.getMaillot(maillotId).thenApplyAsync(maillot -> ok(toJson(new MaillotDTO(maillot)))).exceptionally(ex -> {
+        return cache.getOrElseUpdate("maillots/maillot/"+maillotId, () ->maillotRepository.getMaillot(maillotId).thenApplyAsync(maillot -> ok(toJson(new MaillotDTO(maillot)))).exceptionally(ex -> {
             Result res;
             if(ExceptionUtils.getRootCause(ex).getClass().getSimpleName().equals(NULLPOINTEREXCEPTION)){
                 res = badRequest("No specific maillot is set in DB for this id.");
@@ -63,6 +67,6 @@ public class MaillotController extends Controller {
                 res = internalServerError(ex.getMessage());
             }
             return res;
-        });
+        }), CACHE_DURATION);
     }
 }
