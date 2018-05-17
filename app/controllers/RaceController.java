e8da0d4ed23696e6f5e9c48bb21daf360d977e26
Hawner <dominik_good@hotmail.com>
2018-05-17 11:26:55 +0200

    RaceController Caching

--- app/controllers/RaceController.java
+++ app/controllers/RaceController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import models.Race;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import play.cache.AsyncCacheApi;
+import play.cache.Cached;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repository.interfaces.RaceRepository;
@@ -19,13 +21,14 @@ public class RaceController extends Controller {
     private final RaceRepository raceRepository;
     private static final String INDEXOUTOFBOUNDEXCEPETION = "IndexOutOfBoundsException";
     private static final String NORESULTEXCEPTION = "NoResultException";
+    private static final int CACHE_DURATION = 10;
+    private final AsyncCacheApi cache;
 
     @Inject
-    public RaceController(RaceRepository raceRepository) {
-        this.raceRepository = raceRepository;
-    }
+    public RaceController(RaceRepository raceRepository, AsyncCacheApi cache) { this.raceRepository = raceRepository; this.cache = cache; }
 
     @ApiOperation(value ="get all races", response = Race.class, responseContainer = "List")
+    @Cached(key="races", duration = CACHE_DURATION)
     public CompletionStage<Result> getAllRaces() {
         return raceRepository.getAllRaces().thenApplyAsync(races -> ok(toJson(races.collect(Collectors.toList())))).exceptionally(ex -> {
             Result res;
@@ -40,7 +43,7 @@ public class RaceController extends Controller {
 
     @ApiOperation(value ="get race by id", response = Race.class)
     public CompletionStage<Result> getRace(Long raceId) {
-        return raceRepository.getRace(raceId).thenApplyAsync(race -> ok(toJson(race))).exceptionally(ex -> {
+        return cache.getOrElseUpdate("race/"+raceId, () -> raceRepository.getRace(raceId).thenApplyAsync(race -> ok(toJson(race))).exceptionally(ex -> {
             Result res;
             if(ExceptionUtils.getRootCause(ex).getClass().getSimpleName().equals(NORESULTEXCEPTION)){
                 res = badRequest("Race with id: " + raceId + " is not available in DB.");
@@ -48,6 +51,6 @@ public class RaceController extends Controller {
                 res = internalServerError(ex.getMessage());
             }
             return res;
-        });
+        }), CACHE_DURATION);
     }
 }
